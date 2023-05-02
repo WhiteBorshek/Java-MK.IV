84df271e61bea4bcb2d928b101498defc8b93140
WhiteBorshek <121310443+WhiteBorshek@users.noreply.github.com>
2023-05-02 16:34:59 +0500

    Add files via upload

---
+++ HometaskXI.java
@@ -0,0 +1,27 @@
+package Hometasks;
+
+//Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+import java.util.LinkedList;
+
+
+public class HometaskXI {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(getRandomNumber());
+        }
+        System.out.println(ll);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(ll.getLast() + " ");
+            ll.removeLast();
+        }
+    }
+
+    public static int getRandomNumber() {
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
+}
